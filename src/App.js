5c8f852b6a7c0f6d7576202fc4d1f475ad148779
Vaibhav sisodiya <sisodiya.vaib215@gmail.com>
2022-04-01 12:01:10 +0000

    Added Next button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import MemesList from "./Components/MemesList";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import { useState } from "react";
 function App() {
   const [mode, setMode] = useState("dark")
   return (
     <div className="App bg-white dark:bg-slate-900">
-      <Navbar setMode={setMode} mode={mode}/>
-      <MemesList/>
-      <Footer/>
+      <Navbar setMode={setMode} mode={mode} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MemesList />}/>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- src/Components/MemesList.js
+++ src/Components/MemesList.js
@@ -1,42 +1,52 @@
 import React, { Component } from "react";
 import MemeItem from "./MemeItem";
 import CircularProgress from '@mui/material/CircularProgress';
+import { Button } from "@mui/material";
 export default class MemesList extends Component {
-  
+
   constructor() {
     super();
     this.state = {
-      memes:[],
+      memes: [],
       loading: true,
     }
   }
-  async componentDidMount(){
-    let url="https://meme-api.herokuapp.com/gimme/45"
+  fetchData = async () => {
+    let url = "https://meme-api.herokuapp.com/gimme/9"
     let data = await fetch(url)
-    let parsedData = await data.json()
-    this.setState({memes:parsedData.memes,loading:false})
+    return await data.json()
+  }
+  async componentDidMount() {
+    const data = await this.fetchData()
+    this.setState({ memes: data.memes, loading: false })
+  }
+
+  handleMoreMemes = async () => {
+    const data = await this.fetchData()
+    this.setState({ memes: data.memes, loading: false })
   }
   render() {
     return (
       <div>
         <section className="text-blue-400 body-font z-0">
-          <div className="container px-5 py-24 mx-auto">
+          <div className="container px-5 pt-24 md:py-24 mx-auto">
             <div className="flex flex-col text-center w-full mb-20">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4">
+              <h1 id="head" className="sm:text-3xl text-2xl font-medium title-font mb-4">
                 Latest memes ahead
               </h1>
             </div>
-            {this.state.loading?
-            <div className="flex justify-center h-[40vh] items-center">
-              <CircularProgress/> 
-            </div>: <br/>}
+            {this.state.loading ?
+              <div className="flex justify-center h-[40vh] items-center">
+                <CircularProgress />
+              </div> : <br />}
             <div className="flex flex-wrap -m-4">
-              {this.state.memes.map((e,i)=>{
-                return(
-                  <MemeItem key={i} imgUrl={e.url} ups="Todo"/>
+              {this.state.memes.map((e, i) => {
+                return (
+                  <MemeItem key={i} imgUrl={e.url} ups="Todo" />
                 )
               })}
             </div>
+            <Button variant="contained" onClick={this.handleMoreMemes}><a href="#head"> Load More...</a></Button>
           </div>
         </section>
       </div>
